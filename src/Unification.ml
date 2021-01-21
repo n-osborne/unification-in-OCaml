de3e1230cfec45c9c2feebae17611c51d0e60684
Nicolas Osborne <nicolas.osborne.etu@univ-lille.fr>
2021-01-22 00:15:15 +0100

    Robinson algorithm done

--- src/Unification.ml
+++ src/Unification.ml
@@ -17,24 +17,27 @@ struct
     | V of TS.v
     | C of TS.c * term list
 
+  (** We need decidable equality between terms **)
   let rec t_eq (t1: term) (t2: term): bool =
     match t1, t2 with
     | V n1, V n2 -> TS.v_eq n1 n2
     | C (c1, l1), C (c2, l2) ->
       TS.c_eq c1 c2 &&
       List.for_all (fun (x1, x2) -> t_eq x1 x2) (List.combine l1 l2)
-    | _ -> false
+    | _, _ -> false
 
+  (** Check whether a term is a variable *)
   let is_var: term -> bool = function
     | V _ -> true
     | _ -> false
 
+  (** Get the variable name is ther is one **)
   let to_var: term -> TS.v option =
     function
     | V n -> Some n
     | _   -> None
       
-  (** check whether the constants have the right arity **)
+  (** Check whether the constants have the right arity **)
   let rec check: term -> bool =
     function
     | V _ -> true
@@ -62,7 +65,7 @@ struct
     if S.mem v s then S.find v s else V v
 
   (** A trivial binding is a binding of a variable name to a variable
-     term build with this name **)
+      term build with this name **)
   let is_trivial (v: TS.v): term -> bool =
     function
     | V n -> TS.v_eq v n
@@ -73,21 +76,21 @@ struct
     S.filter (fun v t -> not (is_trivial v t))
       
   (** It is nice to get the variable names into a set (which is
-     possible as the variable name type is an OrderedType **)
+      possible as the variable name type is an OrderedType **)
   module V = Set.Make(struct type t = TS.v let compare = TS.v_cmp end)
 
   (** To apply a subst to a term is to travers this term
-     replacing all the variables by the term they are binded with in
-     the subst **)
+      replacing all the variables by the term they are binded with in
+      the subst **)
   let apply (s: subst) : term -> term = trav (get s)
 
   (** The domain of a subst is the set of all the variable
-     names for which a binding is defined and not trivial **)
+      names for which a binding is defined and not trivial **)
   let dom (s: subst) : V.t =
     V.of_seq (Seq.map fst (S.to_seq (filter_trivials s)))
 
   (** The range of a subsitution is the list of terms that we can get
-     from it **)
+      from it **)
   let range (s: subst) : term list =
     List.of_seq (Seq.map snd (S.to_seq s))
 
@@ -96,9 +99,9 @@ struct
     V.of_list (List.concat_map vars (range s))
 
   (** The restiction of a subst s to a set x For every element
-     n of the intesection of x and the domain of s, we add the binding
-     of n in s to a new subst that is build up from an empty
-     one. **)
+      n of the intesection of x and the domain of s, we add the binding
+      of n in s to a new subst that is build up from an empty
+      one. **)
   let restict (s: subst) (x: V.t): subst =
     V.fold (fun n -> S.add n (S.find n s)) (V.inter x (dom s)) S.empty
       
@@ -140,6 +143,51 @@ struct
   (** A subst s is a unifier of two terms if the application of
       s render the two terms equals **)
   let is_unifier (l: term) (r: term) (s: subst): bool =
-    t_eq (apply s l) (apply s r)  
+    t_eq (apply s l) (apply s r)
+
+
+  module Robinson =
+  struct
+
+    let rec disagreement_pair (l: term) (r: term): (term * term) option =
+      match l, r with
+        | V n, V m -> if TS.v_eq n m then None else Some (V n, V m)
+        | V n, C (f, a) -> Some (V n, C (f, a))
+        | C (f, a), V n -> Some (C (f, a), (V n))
+        | C (f, a), C (g, b) ->
+          if TS.c_eq f g
+          then disagreement_pair_list a b
+          else Some (C (f, a), C (g, b))
+    and disagreement_pair_list (a: term list) (b: term list): (term * term) option =
+      match a, b with
+      | [], [] -> None
+      | a :: aa , b :: bb ->
+        (match disagreement_pair a b with
+        | None -> disagreement_pair_list aa bb
+        | Some (l, r) -> Some (l, r))
+      | _, _ -> None
+
+    let simple (l, r: term * term): bool =
+      match to_var l with
+      | None -> (match to_var r with
+          | None -> true
+          | Some v -> not (List.mem v (vars l)))
+      | Some v -> not (List.mem v (vars r))
+
+    let rec unify (l, r: term * term) (s: subst): subst option =
+      match disagreement_pair l r with
+      | None -> Some s
+      | Some (l', r') ->
+        if simple (l', r')
+        then match unify (l', r') S.empty with
+          | None -> None
+          | Some s' ->
+            let s'' = compose s s' in
+            unify (apply s'' l, apply s'' r) s''
+        else None
+    
+  end
+                                             
+  
 end
 
